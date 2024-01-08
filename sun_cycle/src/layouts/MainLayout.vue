26fa2146413265da983ff68df862b3982be129bf
OWEN LANG <owenlang66@gmail.com>
2024-01-08 13:42:31 -0800

    main

--- sun_cycle/src/layouts/MainLayout.vue
+++ sun_cycle/src/layouts/MainLayout.vue
@@ -15,7 +15,7 @@
           Quasar App
         </q-toolbar-title>
 
-        <div>Quasar poop</div>
+        <div>Gordon secretly has a cactus up his butt always</div>
       </q-toolbar>
     </q-header>
 
